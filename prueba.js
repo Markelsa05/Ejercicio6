c7ce12b392d875c8e4fb0859306eb300c0315444
Markelsa05 <msanchezg16@educantabria.es>
2024-09-18 19:35:15 +0200

    Primer Commit

---
+++ prueba.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo");
+a = prompt("Dame un numero del 1 al 10, Numerico porfavor");
+console.log(`El valor introdducido a sido: ${a}`);
+b = "El valor introducido ha sido " +a;
+console.log(b);
